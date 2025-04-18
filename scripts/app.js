c7e9607bf65b0b4daf858403944c9f3c56705c8a
achirame2007 <atharvachirame2007@gmail.com>
2025-04-18 15:31:19 -0400

    Updated css, html and js file

--- scripts/app.js
+++ scripts/app.js
@@ -2,7 +2,12 @@ const app = Vue.createApp({
     data() {
         return{
             posts:  [],
-            weather: []
+            weather: [],
+            city: 'London',
+            province: 'Ontario',
+            country: 'Canada',
+            word:'',
+            define: []
         }
     },
     created(){
@@ -12,8 +17,7 @@ const app = Vue.createApp({
     methods:
     {
         fetchdata (){
-            fetch('https://comp6062.liamstewart.ca/random-user-profile'
-            )
+            fetch('https://comp6062.liamstewart.ca/random-user-profile')
             .then(response => {
                 if(response.ok){
                     return response.json();
@@ -29,18 +33,55 @@ const app = Vue.createApp({
                 console.log('Total Failure');
             });
         },
+
         getWeather() {
-            fetch('https://comp6062.liamstewart.ca/weather-information?city=Toronto&province=Ontario&country=Canada')
-              .then(response => {
-                if (response.ok) return response.json();
-                else console.log('Error fetching weather');
-              })
-              .then(data => {
-                this.weather = data;
-                console.log(weather);
-              })
-              .catch(error => console.log('Weather fetch failed'));
+            let city = this.city ?? 'London';
+            let province = this.province ?? 'Ontario';
+            let country = this.country ?? 'Canada';
+
+            if(city == '' || province == ''|| country =='')
+            {
+                return this.weather = {message: "Please fill all fields"};
             }
+            else
+            {
+            fetch('https://comp6062.liamstewart.ca/weather-information?city=' + city  + '&province=' + province + '&country=' + country)
+            .then(response => {
+                if(response.ok) {
+                    return response.json();
+                }
+                else {
+                    console.log('Error...Please try again.');
+                }
+            })
+            .then(data => {
+                this.weather = data;
+            })
+              .catch(error => {
+                console.log('Weather fetch failed');
+              });
+            }},
+        
+        getdefination(){
+            let word = this.word ?? '';
+            if(word == '')
+            {
+                return this.define = {message:"Please Enter the word"};
+            }
+            else
+            {
+            fetch('https://comp6062.liamstewart.ca/define?word=' + word)
+            .then(response => {
+                if(response.ok) {
+                    return response.json();
+                }
+                else {
+                    console.log('An Error Occured');
+                }
+            })
+            .catch(error => console.log('No word found'));
+            }
+        }
     }
 });
 
